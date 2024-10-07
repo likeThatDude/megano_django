786926f708232c59bf2406148484dd248b3ab0e5
munchen777.eth <isak.georgiy@mail.ru>
2024-10-07 12:52:49 +0300

    3 add login, register, profile views

--- website/account/views.py
+++ website/account/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def login_view(request: HttpRequest):
+    return render(request, "account/login.html")
+
+
+def register_view(request: HttpRequest):
+    return render(request, "account/register.html")
+
+
+def profile_view(request: HttpRequest):
+    return render(request, "account/profile.html")
