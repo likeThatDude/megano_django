e969485026c108d9f19809da563329d1766334ce
vladimir <vovka1998@gmail.com>
2024-10-16 09:00:11 +0300

    moved model Review from account in catalog

--- website/catalog/models.py
+++ website/catalog/models.py
@@ -1,4 +1,5 @@
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 from django.db import models
 
 from catalog.utils import product_images_directory_path
@@ -82,3 +83,21 @@ class ProductImage(models.Model):
     image = models.ImageField(
         upload_to=product_images_directory_path, verbose_name=_("Image product")
     )
+
+
+class Review(models.Model):
+    """
+    Модель отзыва
+    product: товар к которому относится данный отзыв
+    user: пользователь, который оставил отзыв #TODO после создания модели CustomUser заменить связанную
+    text: текст отзыва
+    created_at: время создания отзыва (создается автоматически)
+    """
+
+    product = models.OneToOneField(
+        Product, on_delete=models.CASCADE, verbose_name=_("Product")
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("User"))
+    text = models.TextField(verbose_name=_("Text"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created at"))
+
