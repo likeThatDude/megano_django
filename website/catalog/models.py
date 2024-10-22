7232740ba5a5baa278e00d4c9ef033762a020089
Bekkhan <israpal@bk.ru>
2024-10-22 13:54:58 +0300

    fixed custom user's app_label

--- website/catalog/models.py
+++ website/catalog/models.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 from django.db.models import ManyToManyField
 from django.utils.translation import gettext_lazy as _
 
-from website import settings
 from .utils import product_images_directory_path, seller_image_directory_path, product_image_directory_path
 
 
--- website/website/settings.py
+++ website/website/settings.py
@@ -139,6 +139,6 @@ MEDIA_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "account.CustomUser"
+AUTH_USER_MODEL = "auth.CustomUser"
 
 LOGIN_URL = reverse_lazy("core:login")
