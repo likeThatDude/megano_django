9c01bffcf672ac27028af4c4e0c4123ca1e146c5
munchen777.eth <isak.georgiy@mail.ru>
2024-10-28 18:33:45 +0300

    add order model to admin

--- website/order/admin.py
+++ website/order/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "profile",
+        "delivery_city",
+        "delivery_address",
+        "status",
+        "payment_method",
+        "delivery_method",
+        "archived",
+        "created_at",
+    )
+    ordering = ("pk",)
+    list_editable = [
+        "delivery_city",
+        "delivery_address",
+        "status",
+        "payment_method",
+        "delivery_method",
+        "archived",
+    ]
