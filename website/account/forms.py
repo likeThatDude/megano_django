0f922ff090d5594c340af1fe23a2bf1255d9e779
munchen777.eth <isak.georgiy@mail.ru>
2024-10-28 22:10:19 +0300

    add form for edit profile

--- website/account/forms.py
+++ website/account/forms.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-from .models import CustomUser
+from .models import CustomUser, Profile
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,3 +16,20 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email",)
+
+
+class ProfileChangeForm(forms.ModelForm):
+    """
+    Форма для редактирования профиля (поля профиля + поля пользователя)
+
+    email - почта пользователя
+    first_name - имя в профиле
+    last_name - фамилия в профиле
+    patronymic - отчество в профиле (необязательно)
+    phone - номер телефона профиля
+    photo - фотография профиля
+    """
+
+    class Meta:
+        model = Profile
+        fields = ["first_name", "last_name", "patronymic", 'phone', 'photo']
