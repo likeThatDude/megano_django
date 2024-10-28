f4a663742f0087eea0b976412863c3d0d34f3f0a
munchen777.eth <isak.georgiy@mail.ru>
2024-10-28 22:13:25 +0300

    implement profile view and personal cabinet view

--- website/account/views.py
+++ website/account/views.py
@@ -1,10 +1,14 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.db import transaction
+from django.views.generic import CreateView, DetailView, UpdateView
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileChangeForm
 from .models import Profile
 
 
@@ -26,10 +30,11 @@ class RegisterView(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        # Реализовать создание профиля пользователя
-
-        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password1")
+        user = authenticate(self.request, email=email, password=password)
+        if user:
+            login(self.request, user=user)
 
         return response
 
@@ -37,5 +42,73 @@ class RegisterView(CreateView):
         return reverse_lazy("core:index")
 
 
-def profile_view(request: HttpRequest):
-    return render(request, "account/profile.html")
+class ProfileView(LoginRequiredMixin, UpdateView):
+    """
+    CBV для обновления профиля пользователя
+
+    model - модель, с которой работаем
+    form_class - форма для редактирования профиля
+
+    """
+    template_name = "account/profile.html"
+    context_object_name = "profile"
+    model = Profile
+    form_class = ProfileChangeForm
+
+    def get_object(self, queryset=None):
+        """
+        Получаем профиль пользователя
+        """
+        return self.request.user.profile
+
+    def get_context_data(self, **kwargs):
+        """
+        Передаем в форму в качестве инстанса профиль пользователя
+        """
+        context = super().get_context_data(**kwargs)
+        context["profile_form"] = ProfileChangeForm(instance=self.request.user.profile)
+        return context
+
+    def form_valid(self, form):
+        if form.is_valid():
+            profile = form.save()
+            user = profile.user
+            user.email = form.cleaned_data["email"]
+            user.save()
+            return redirect("account:profile")
+        return redirect(self.request.path)
+
+    def get_success_url(self):
+        return reverse_lazy("account:profile")
+
+
+class PersonalCabinet(LoginRequiredMixin, DetailView):
+    """
+
+    CBV личного кабинета профиля пользователя
+    В шаблоне присутствуют ссылки на профиль,
+    на все заказы профиля пользователя
+
+    """
+    template_name = "account/personal_cabinet.html"
+    context_object_name = "profile"
+    model = Profile
+
+    def get_context_data(self, **kwargs):
+        """
+        Атрибуты:
+        profile - профиль пользователя
+        last_order - последний заказ
+        """
+        context = super().get_context_data(**kwargs)
+        profile = Profile.objects.get(user=self.request.user)
+
+        # Передаем последний заказ текущего профиля
+        profile_orders = profile.orders.all()
+        context["profile"] = profile
+        context["last_order"] = profile_orders.order_by('-created_at').first()
+        return context
+
+    def get_object(self, queryset=None):
+        """ Возвращаем профиль пользователя """
+        return Profile.objects.get(user=self.request.user)
