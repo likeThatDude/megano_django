f719fc409083402827f7b35bee596be4ad9f8a76
vladimir <vovka1998@gmail.com>
2024-11-02 14:47:48 +0300

    moved func getCookie

--- website/cart/static/cart/js/add_product_cart.js
+++ website/cart/static/cart/js/add_product_cart.js
@@ -1,20 +1,3 @@
-// Функция для получения куков
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-
 // Функция для добавления товара в корзину
 async function addProductInCart(url) {
     try {
