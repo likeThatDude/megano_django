c3a8797b670d47a0108e33f8692bfbf82d125d09
munchen777.eth <isak.georgiy@mail.ru>
2024-11-05 22:03:53 +0300

    add orders history view

--- website/order/urls.py
+++ website/order/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import order_create_view
 from .views import order_detail_view
+from .views import OrdersHistoryListView
 
 app_name = "order"
 
 urlpatterns = [
     path("create/", order_create_view, name="order_create"),
     path("detail/", order_detail_view, name="order_detail"),
+    path("orders_history/", OrdersHistoryListView.as_view(), name="orders-history"),
 ]
