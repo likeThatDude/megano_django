c6ea12375c5e00b9788b956d5d70919d4d52fb4f
munchen777.eth <isak.georgiy@mail.ru>
2024-11-09 12:34:16 +0300

    change url path for order details

--- website/order/urls.py
+++ website/order/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from .views import order_create_view
-from .views import order_detail_view
+from .views import OrderDetailView
 from .views import OrdersHistoryListView
 
 app_name = "order"
 
 urlpatterns = [
     path("create/", order_create_view, name="order_create"),
-    path("detail/", order_detail_view, name="order_detail"),
+    path("order_detail/<int:pk>", OrderDetailView.as_view(), name="order_detail"),
     path("orders_history/", OrdersHistoryListView.as_view(), name="orders-history"),
 ]
