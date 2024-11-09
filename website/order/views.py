edc0560aaa8677f181a26f2680b8d29e7d486255
munchen777.eth <isak.georgiy@mail.ru>
2024-11-09 12:35:57 +0300

    add order details view

--- website/order/views.py
+++ website/order/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpRequest
 from django.db.models import F, Sum
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from .models import Order
 
@@ -10,8 +11,29 @@ def order_create_view(request: HttpRequest):
     return render(request, "order/order.html")
 
 
-def order_detail_view(request: HttpRequest):
-    return render(request, "order/order-detail.html")
+class OrderDetailView(PermissionRequiredMixin, DetailView):
+    """
+    Представление для деталей заказа
+
+    Доступно тем пользователям, у которых есть соответствующие разрешения
+
+    Атрибуты:
+        permission_required: Необходимые разрешения пользователю
+        template_name (str): Путь к шаблону, который будет использоваться для отображения деталей заказа
+        queryset (Queryset): Queryset конкретного заказа
+        context_object_name (str): Имя объекта контекста для передачи в шаблон.
+    """
+    permission_required = "order.view_order"
+    model = Order
+    context_object_name = "order"
+    template_name = "order/order_details.html"
+
+    def get_object(self, queryset=None):
+        """
+        Получаем pk заказа и возвращаем соответствующий заказ
+        """
+        pk = self.kwargs.get("pk")
+        return Order.objects.get(pk=pk)
 
 
 class OrdersHistoryListView(ListView):
