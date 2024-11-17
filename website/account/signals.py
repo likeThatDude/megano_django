db187ed1b3bfade36e83305bb6a3532be3ad7b51
munchen777.eth <isak.georgiy@mail.ru>
2024-11-17 13:07:28 +0300

    add signal to create profile with superuser creation

---
+++ website/account/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import CustomUser, Profile
+
+
+@receiver(post_save, sender=CustomUser)
+def create_superuser_profile(sender, instance, created, **kwargs):
+    """
+    Сигнал для создания профиля только для суперпользователей.
+    """
+    if created and instance.is_superuser:
+        # Создаем профиль только для суперпользователя
+        Profile.objects.create(user=instance)
