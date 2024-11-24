5fe5e3baea98416d3e4ac492e5ce424c3b5a8f1e
munchen777.eth <isak.georgiy@mail.ru>
2024-11-24 15:50:42 +0300

    add path to update discount view

--- website/discount/urls.py
+++ website/discount/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from .views import DiscountCreateView
+from .views import DiscountUpdateView
+
 
 app_name = "discount"
 
 urlpatterns = [
     path("create/", DiscountCreateView.as_view(), name="discount-create"),
+    path("update/<str:slug>/", DiscountUpdateView.as_view(), name="discount-update"),
 ]
