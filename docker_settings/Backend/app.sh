658bdc951786115fb333bb7160e0c3376717fed2
Gorbatenko Ivan <1995van95@gmail.com>
2024-11-26 22:25:17 +0300

    add superuser creation on server container startup

--- docker_settings/Backend/app.sh
+++ docker_settings/Backend/app.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Superuser email: ${DJANGO_SUPERUSER_EMAIL}"
+echo "Superuser password: ${DJANGO_SUPERUSER_PASSWORD}"
+echo "Superuser login: ${DJANGO_SUPERUSER_LOGIN}"
+
+
 if [[ "${1}" == "celery" ]]; then
   apt update
   apt install -y postgresql postgresql-contrib
@@ -11,6 +16,21 @@ elif [[ "${1}" == "app" ]]; then
   python manage.py makemigrations
   python manage.py migrate auth
   python manage.py migrate
+
+  echo "Creating superuser..."
+  python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+
+User = get_user_model()
+if not User.objects.filter(email="${DJANGO_SUPERUSER_EMAIL}").exists():
+    User.objects.create_superuser(
+        email="${DJANGO_SUPERUSER_EMAIL}",
+        password="${DJANGO_SUPERUSER_PASSWORD}",
+        login="${DJANGO_SUPERUSER_LOGIN}"
+    )
+EOF
+  echo "Superuser ${DJANGO_SUPERUSER_LOGIN} is created..."
 
   gunicorn --config ./config/gunicorn.conf.py website.wsgi:application
  fi
