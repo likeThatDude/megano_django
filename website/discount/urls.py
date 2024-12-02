9c5a5858d6c89901aa4405ae373a98caec626ddd
vladimir <vovka1998@gmail.com>
2024-12-02 11:38:45 +0300

    created and connected view detail discount

--- website/discount/urls.py
+++ website/discount/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .views import DiscountCreateView
-from .views import DiscountUpdateView
+from .views import DiscountUpdateView, DiscountDetailView
 
 app_name = "discount"
 
 urlpatterns = [
     path("create/", DiscountCreateView.as_view(), name="discount-create"),
+    path("detail/<str:slug>/", DiscountDetailView.as_view(), name="discount-detail"),
     path("update/<str:slug>/", DiscountUpdateView.as_view(), name="discount-update"),
 ]
