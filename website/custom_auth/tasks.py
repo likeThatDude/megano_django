cd73130640ac462f69c353ef5b5f3c8e5403462c
munchen777.eth <isak.georgiy@mail.ru>
2024-12-14 10:22:54 +0300

    add periodical task happy birthday email

--- website/custom_auth/tasks.py
+++ website/custom_auth/tasks.py
@@ -68,3 +68,50 @@ def notify_user_after_register(self, user_pk):
     except Exception as error:
         print(f"Не удалось выполнить задачу notify_user_after_register")
         raise self.retry(exc=error)
+
+
+@app.task
+def send_user_happy_birthday():
+    """
+    Запланированная задача отправки поздравления с днем рождения пользователю
+    Проверка производится каждый день.
+    Если у пользователя день рождение, отправляем электронное письмо
+
+    """
+    subject: str = "С днем рождения от MEGANO"
+    from_email = EMAIL_HOST_USER
+    users: QuerySet[CustomUser] = CustomUser.objects.all()
+
+    for user in users:
+        try:
+            current_date = datetime.today().date()
+
+            if user.birthday == current_date:
+                html_content = render_to_string(
+                    "custom_auth/happy_birthday_email.html",
+                    {
+                        "username": user.login,
+                        "company": "MEGANO",
+                        "protocol": HTTP_PROTOCOL,
+                        "domain": SERVER_DOMAIN,
+                        "year": datetime.now().year,
+                    }
+                )
+                plain_message = strip_tags(html_content)
+                email: EmailMultiAlternatives = EmailMultiAlternatives(
+                    subject,
+                    plain_message,
+                    from_email,
+                    [user.email]
+                )
+                email.attach_alternative(html_content, "text/html")
+
+                try:
+                    email.send()
+                    print("Письмо успешно отправлено.")
+
+                except Exception as error:
+                    print(f"Ошибка при отправке письма: {error}")
+
+        except Exception as error:
+            print(f"Произошла ошибка! | Error: {error}")
